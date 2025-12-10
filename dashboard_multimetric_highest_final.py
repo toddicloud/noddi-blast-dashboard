1f57e0b797375713bf6c9350d92edb04579f282b
toddicloud <richards.todd1@icloud.com>
2025-12-10 09:42:39 -0800

    Update dashboard_multimetric_highest_final.py

--- dashboard_multimetric_highest_final.py
+++ dashboard_multimetric_highest_final.py
@@ -7,37 +7,6 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["app_password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        st.text_input(
-            "Enter password to access the dashboard:",
-            type="password",
-            on_change=password_entered,
-            key="password",
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input(
-            "Enter password to access the dashboard:",
-            type="password",
-            on_change=password_entered,
-            key="password",
-        )
-        st.error("❌ Incorrect password")
-        return False
-    else:
-        return True
-
-
-if not check_password():
-    st.stop()
 
 
 st.set_page_config(page_title="Multi-Metric NODDI + Blast Dashboard", layout="wide")
